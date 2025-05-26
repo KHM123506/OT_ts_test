2c991a71ea72f0a6873732d4d51c3ddd743e2b1c
khm <kharvaach.mergen@gmail.com>
2025-05-26 17:27:09 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+API_TOKEN = "7352936643:AAE6VU2gzl3URACE6ik3oAbq5SgK0Itdg2g"  # Bot token
+CREDS_FILE = 'credentials.json'
+SCOPES = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+SHEET_URL_EMPLOYEES = 'https://docs.google.com/spreadsheets/d/1q2WMNg6_lrD6C477WuIh1WSEe8sCy7ruqhkiH2MkiNI/edit?usp=sharing'
+SHEET_URL_ATTENDANCE = 'https://docs.google.com/spreadsheets/d/1_X6sQhXDoneXMXGhtml6M7aa6bGd-B6M_0NZJwEZXns/edit?usp=sharing'
+SHEET_URL_LOCATION = 'https://docs.google.com/spreadsheets/d/1yi7a8y7Ahtkx8ipj9lYNN7iGnLU2Yy5I_9Pq8O--ujI/edit?usp=sharing'
+WORKSHEET_NAME = "offices"
